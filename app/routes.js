922ce6dc3d7766118d4294f19a93dacde7605ec4
Matias De Santi <matias.desanti@wolox.com.ar>
2019-01-08 11:09:08 -0300

    Adds metrics endpoint

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,15 @@
-// const controller = require('./controllers/controller');
+const { getSummary, getContentType } = require('@promster/express');
 
 exports.init = app => {
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
+  app.use('/metrics', (req, res) => {
+    req.statusCode = 200;
+
+    res.setHeader('Content-Type', getContentType());
+    res.end(getSummary());
+  });
   app.get('/health', [], (req, res, next) => res.status(200).end());
   app.get('/203', [], (req, res, next) => res.status(203).end());
   app.get('/internalError', [], (req, res, next) => res.status(500).end());
